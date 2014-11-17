3ca3f3197971c95303753328481e031e6bd09b5d
goran <gboban70@gmail.com>
2014-11-17 15:39:13 +0100

    first commit (structure)

---
+++ src/com/gmail/gboban70/awtanimation/AWTAnimationApp.java
@@ -0,0 +1,10 @@
+package com.gmail.gboban70.awtanimation;
+
+public class AWTAnimationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/gmail/gboban70/awtanimation/AnimationFrame.java
@@ -0,0 +1,12 @@
+package com.gmail.gboban70.awtanimation;
+
+import java.awt.Frame;
+
+public class AnimationFrame extends Frame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
