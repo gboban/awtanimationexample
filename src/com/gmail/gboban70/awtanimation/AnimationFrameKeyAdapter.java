3ea76e6a03710a163cbdd953d154386c2c59fc29
goran <gboban70@gmail.com>
2014-11-19 00:59:04 +0100

    added Drawable to AnimationFrameKeyAdapter

--- src/com/gmail/gboban70/awtanimation/AnimationFrameKeyAdapter.java
+++ src/com/gmail/gboban70/awtanimation/AnimationFrameKeyAdapter.java
@@ -1,17 +1,23 @@
 package com.gmail.gboban70.awtanimation;
 
-import MasaZombieFrame;
-
-import java.awt.Frame;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class AnimationFrameKeyAdapter extends KeyAdapter {
+	private Drawable drawable = null;
+	
+	public Drawable getDrawable() {
+		return drawable;
+	}
+
+	public void setDrawable(Drawable drawable) {
+		this.drawable = drawable;
+	}
+	
 	public void keyPressed(KeyEvent ke){
 		AnimationFrame frm = (AnimationFrame)ke.getSource();
 		
 		int kc = ke.getKeyCode();
-		//System.out.println(kc);
 		
 		frm.keyPressed(ke);
 	}
